396b5b16dd4574af94abe2d711d4cc761ce461f2
Rui Almeida <ryuumaru27@gmail.com>
2012-09-26 21:17:36 +0100

    Proper packages

---
+++ ns-group42/uMADs/src/test/console/uMadsTest.java
@@ -0,0 +1,24 @@
+package test.console;
+
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author ei10139
+ */
+public class uMadsTest {
+    
+    public uMadsTest() {
+    }
+    
+    @Test
+    public void testAnalyze() {
+        System.out.println("testing analyze");
+        uMads instance = new uMads();
+        instance.analyze('X');
+    }
+}
