4db6374a74f3278a1b6999052593b4afe13f1020
Tiago Casanova <tiago.casanova.pt@gmail.com>
2012-09-26 19:32:30 +0100

    -read from file

--- ns-group3/src/Tab.java
+++ ns-group3/src/Tab.java
@@ -1,16 +1,47 @@
-import java.awt.Point;
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 
 public class Tab {
-	private HashMap<Point, Character> map = new HashMap<Point, Character>();
-	
-	public void change(int x, int y, char f){
-		Point position = new Point(x, y);
-		map.put(position, f);
-	}
-	
-	public HashMap<Point, Character> getmap(){
-		return map;
+
+	public void readFromFile(String fileName)
+	{
+		try{
+			// Open the file 
+			FileInputStream fstream = new FileInputStream(fileName);
+
+
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+
+			int lineCounter = 0;
+			//Read File 
+			String tab = "";
+			while ((strLine = br.readLine()) != null)   {
+				tab += strLine+"\n";
+				lineCounter+=1;
+			}
+			System.out.println (tab);
+			String[] lines = tab.split("\n");
+			
+			for (int i=lineCounter; i!= 0; i--)   {
+				
+				for(int j=1; j!=lineCounter+1; j++)
+				{
+					//map.change(j,i,lines[lineCounter-i].charAt(j));
+				}
+			}
+			
+			//Close the input stream
+			in.close();
+			fstream.close();
+			
+		}catch (Exception e){//Catch exception if any
+			System.out.println("Error: " + e.getMessage());
+		}
+			  
 	}
 }
