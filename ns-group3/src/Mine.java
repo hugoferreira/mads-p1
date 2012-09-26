57edaa3735a9d1012c515e3930a908adb02663a2
Pedro Machado <pedromfmachado@gmail.com>
2012-09-26 18:52:19 +0100

    ciclo de jogo iniciado

--- ns-group3/src/Mine.java
+++ ns-group3/src/Mine.java
@@ -1,12 +1,30 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class Mine {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
+		
+		while(true){ // verify if there are still diamons left
+			
+			System.out.print("Direction to move:");
+			// wait for input
+			Scanner scan = new Scanner (System.in);
+			char userInput = scan.next().charAt(0);
+			
+			// robot's movement
+			
+			// tab update
+			
+			
+		}
 	}
 
 }
