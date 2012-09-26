fae1e173d2b8daeba6dcb86bc723212f6dfa83da
Bruno Nova <ei08109@fe.up.pt>
2012-09-26 14:54:18 +0100

    Criado projecto no Netbeans.

---
+++ ds-group1/MADS/src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bruno
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.err.println("Hello World!");
+	}
+}
