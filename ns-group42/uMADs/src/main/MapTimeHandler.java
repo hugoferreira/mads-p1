8bb7519387e45a3ad7099e0e761f2e59f0d39240
Lu�s Fonseca <invent00r@gmail.com>
2012-09-26 23:22:34 +0100

    Added MapTimeHandler

---
+++ ns-group42/uMADs/src/main/MapTimeHandler.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.util.Stack;
+
+public class MapTimeHandler {
+	
+	public Stack<Map> past = new Stack<Map>();
+	public Stack<Map> future = new Stack<Map>();
+	
+	
+	public boolean undo()
+	{
+		return future.add(past.pop());
+	}
+	
+	public boolean redo()
+	{
+		if(!canRedo())
+			return false;
+		
+		return past.add(future.pop());
+	}
+	
+	public boolean canRedo()
+	{
+		return future.size() > 0;
+	}
+	
+	public void storeMap(Map m)
+	{
+		past.add(m);
+		future.clear();
+	}
+}
