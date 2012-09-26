1692bc64107c168c08a5572415eb4538eb392ab6
Tiago Babo <tiago.babo@gmail.com>
2012-09-26 19:19:14 +0100

    Adds input class

---
+++ ns-group1/src/layout/Input.java
@@ -0,0 +1,40 @@
+package layout;
+
+import java.util.Scanner;
+
+public class Input {
+	private static Scanner scanner = new Scanner(System.in);
+	
+	// KEY CODES
+	public static int LEFT = 0;
+	public static int UP = 1;
+	public static int RIGHT = 2;
+	public static int DOWN = 3;
+	public static int WAIT = 4;
+	public static int INVALID_MOVE = -1;
+	
+	// AVAILABLE KEYBOARD KEYS
+	public static String LEFT_KEY = "l";
+	public static String UP_KEY = "u";
+	public static String RIGHT_KEY = "r";
+	public static String DOWN_KEY = "d";
+	public static String WAIT_KEY = "w";
+	
+	// READS INPUT FROM KEYBOARD AND RETURNS KEY CODE
+	public static int checkInput()
+	{
+		String input = scanner.next();
+		if(input.equals(LEFT_KEY))
+			return LEFT;
+		else if(input.equals(UP_KEY))
+			return UP;
+		else if(input.equals(RIGHT_KEY))
+			return RIGHT;
+		else if(input.equals(DOWN_KEY))
+			return DOWN;
+		else if(input.equals(WAIT_KEY))
+			return WAIT;
+		
+		return INVALID_MOVE;	
+	}
+}
