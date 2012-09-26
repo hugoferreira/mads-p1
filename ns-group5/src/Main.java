f236021f4cbc6036e05f44b3fe8fefc1f08d8cc8
André <andrenuclear@gmail.com>
2012-09-26 21:17:19 +0100

    Print Map

--- ns-group5/src/Main.java
+++ ns-group5/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 	
-	
+	public static Mine mine = new Mine("");
 	public static Scanner scanner = new Scanner(System.in);
 	
 	/**
@@ -16,6 +16,9 @@ public class Main {
 		while(true) {
 			
 			// exit condition
+			
+			// show map
+			mine.print();
 			
 			// ask user for a command
 			String command = scanner.nextLine();
