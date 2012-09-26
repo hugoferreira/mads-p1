14f7530ba574d87e7fc485ad26f537723ee55104
Tiago Babo <tiago.babo@gmail.com>
2012-09-26 19:29:06 +0100

    Adds player class

---
+++ ns-group1/src/map/Player.java
@@ -0,0 +1,37 @@
+package map;
+
+public class Player {
+	private int pos_x;
+	private int pos_y;
+	private int diamonds;
+	
+	Player(int x, int y) {
+		pos_x = x;
+		pos_y = y;
+		diamonds = 0;
+	}
+	
+	public int getPos_x() {
+		return pos_x;
+	}
+	
+	public void setPos_x(int pos_x) {
+		this.pos_x = pos_x;
+	}
+	
+	public int getPos_y() {
+		return pos_y;
+	}
+	
+	public void setPos_y(int pos_y) {
+		this.pos_y = pos_y;
+	}
+	
+	public int getDiamonds() {
+		return diamonds;
+	}
+	
+	public void addDiamond() {
+		diamonds++;
+	}
+}
