777350453a5a8a33e0c0fe8af284d18109af3318
José Pedro Silva <sobreiro.pedro@gmail.com>
2012-09-26 16:41:08 +0100

    Map

--- ds-group1/src/map/Map.java
+++ ds-group1/src/map/Map.java
@@ -10,6 +10,13 @@ public class Map {
 
 	private ArrayList<ArrayList<Cell>> map;
 	
+	public Map(int sizex, int sizey){
+		map = new ArrayList<ArrayList<Cell>>(sizex);
+		for(int i = 0; i < sizex; i++){
+			map.set(i, new ArrayList<Cell>(sizey));
+		}
+	}
+	
 	public Cell getXY(int x, int y){
 		return map.get(x - 1).get(y - 1);
 	}
@@ -21,25 +28,35 @@ public class Map {
 		try {
 			int x = 0, y = 0;
 			while((line = in.readLine()) != null){
+				
 				char[] lineChars = line.toCharArray();
 				y = 0;
 				for (char c : lineChars) {
 					
 					switch (c) {
 					case '#':
-						
+						map.get(x).set(y, new Wall());
 						break;
 					case '.':
-						
+						map.get(x).set(y, new Earth());
 						break;
 					case 'R':
-						
+						map.get(x).set(y, new Robot());
 						break;
 					case 'x':
-						
+						map.get(x).set(y, new Diamond());
 						break;
 					case 'L':
-						
+						map.get(x).set(y, new ClosedLift());
+						break;
+					case 'O':
+						map.get(x).set(y, new OpenLift());
+						break;
+					case '*':
+						map.get(x).set(y, new Rock());
+						break;
+					case ' ':
+						map.get(x).set(y, new Empty());
 						break;
 					default:
 						break;
