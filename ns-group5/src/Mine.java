6cf27b18f6308dd818e1815792d89a6fbb432d01
Gonzaga Silva <zaga.silva@gmail.com>
2012-09-26 18:47:07 +0100

    Mine Interface

---
+++ ns-group5/src/Mine.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+
+public class Mine {
+	ArrayList<ArrayList<Character>> map;
+	
+	public void readMap(){
+		
+	}
+	
+	public String toString(){
+		return null;
+	}
+	
+	public Character getCell() {
+		return null;
+		
+	}
+	
+	public boolean setCell(int n, int m, Character c) {
+		return false;
+		
+	}
+	
+	public boolean move(int n1, int m1, int n2, int m2) {
+		return false;
+	}
+	
+	public void updateMap() {
+		
+	}
+	
+	public boolean levelUp() {
+		return false;
+	}
+	
+}
