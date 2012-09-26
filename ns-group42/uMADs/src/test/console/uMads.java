820cc40d41c3ca9b03cfa6ba404dc1ef5a3ab46a
Rui Almeida <ryuumaru27@gmail.com>
2012-09-26 21:54:34 +0100

    Moving rocks

--- ns-group42/uMADs/src/test/console/uMads.java
+++ ns-group42/uMADs/src/test/console/uMads.java
@@ -2,6 +2,7 @@ package test.console;
 
 import java.util.Scanner;
 import main.Map;
+import main.Pos;
 
 /**
  *
@@ -37,7 +38,50 @@ public class uMads {
         }
     }
     
-    public void loadMap(String file)
+    public void rockcheck(Map m){
+    	for (int i = 0; i<m.map.length; i++)
+    	{
+    		for (int j = 0; j<m.map.length; j++)
+    		{
+    			if(m.map[i][j]==Pos.ROCK)
+    				if(belowrockcheck(m,i,j)==Pos.EMPTY)
+    					droprock(m,i,j);
+    				else if(belowrockcheck(m,i,j)==Pos.ROCK)
+    					sliderock(m,i,j);
+    		}
+    	}
+    }
+    
+    private void sliderock(Map m, int i, int j) {
+    	if( m.map[i+1][j+1]==Pos.EMPTY){
+		m.map[i+1][j+1]=Pos.ROCK;
+		m.map[i][j]=Pos.EMPTY;
+    	}
+    	else
+    		if( m.map[i-1][j+1]==Pos.EMPTY){
+    			m.map[i-1][j+1]=Pos.ROCK;
+    			m.map[i][j]=Pos.EMPTY;
+    		}
+		
+	}
+
+	private void droprock(Map m, int i, int j) {
+		m.map[i][j]=Pos.EMPTY;
+		m.map[i][j+1]=Pos.ROCK;
+		
+	}
+
+	public  Pos belowrockcheck(Map m, int i, int j) {
+    	
+    		return m.map[i][j+1];
+    	
+    	
+
+
+		
+	}
+
+	public void loadMap(String file)
     {
         // TODO
     }
