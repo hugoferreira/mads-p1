81e86a8fb59f6dc8347b64ac8aeeaa6ad860565f
Lu�s Fonseca <invent00r@gmail.com>
2012-09-26 22:53:09 +0100

    TestMap bonito

--- ns-group42/uMADs/src/test/TestMap.java
+++ ns-group42/uMADs/src/test/TestMap.java
@@ -7,12 +7,12 @@ public class TestMap extends Map{
 
 	public TestMap(){
 		map = new  Pos[][]{	
-				{Pos.WALL, Pos.WALL,Pos.WALL,Pos.WALL,Pos.WALL,Pos.WALL},
-				{Pos.WALL, Pos.EARTH,Pos.EMPTY,Pos.ROCK,Pos.ROBOT,Pos.WALL},
-				{Pos.WALL, Pos.EMPTY,Pos.EMPTY,Pos.DIAMOND,Pos.EARTH,Pos.WALL},
-				{Pos.WALL, Pos.DIAMOND,Pos.EMPTY,Pos.ROCK,Pos.EMPTY,Pos.WALL},
-				{Pos.LIFT_C, Pos.EMPTY,Pos.EMPTY,Pos.EARTH,Pos.DIAMOND,Pos.WALL},
-				{Pos.WALL, Pos.WALL,Pos.WALL,Pos.WALL,Pos.WALL,Pos.WALL}
+				{Pos.WALL, 	Pos.WALL,	Pos.WALL, Pos.WALL,   Pos.WALL,    Pos.WALL},
+				{Pos.WALL, 	Pos.EARTH,	Pos.EMPTY,Pos.ROCK,	  Pos.ROBOT,   Pos.WALL},
+				{Pos.WALL, 	Pos.EMPTY,	Pos.EMPTY,Pos.DIAMOND,Pos.EARTH,   Pos.WALL},
+				{Pos.WALL, 	Pos.DIAMOND,Pos.EMPTY,Pos.ROCK,   Pos.EMPTY,   Pos.WALL},
+				{Pos.LIFT_C,Pos.EMPTY,	Pos.EMPTY,Pos.EARTH,  Pos.DIAMOND, Pos.WALL},
+				{Pos.WALL, 	Pos.WALL,	Pos.WALL, Pos.WALL,   Pos.WALL,	   Pos.WALL}
 			};
 	}
 	
