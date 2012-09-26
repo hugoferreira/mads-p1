218bcb0a963c68003cefb1dd9607e9fb51b1981a
João Estevinho <jp_estevinho@hotmail.com>
2012-09-26 21:22:07 +0100

    Changed position

--- ns-group5/src/Mine.java
+++ ns-group5/src/Mine.java
@@ -5,8 +5,6 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-import com.sun.tools.javac.util.Pair;
-
 
 public class Mine {
 	ArrayList<ArrayList<Character>> map = new ArrayList<ArrayList<Character>>();
@@ -101,16 +99,27 @@ public class Mine {
 	
 	// returns robot position (l, c)
 	// when there's no robot, returns (-1, -1)
-	public Pair<Integer, Integer> getRobotPosition() {
+	public Position getRobotPosition() {
 		for (int i=0; i<map.size(); i++) {
 			for (int j=0; j<map.get(i).size(); j++) {
 				if (map.get(i).get(j).equals('F')) {
-					return new Pair<Integer, Integer>(i, j);
+					return new Position(i, j);
 				}
 
 			}
 		}
-		return new Pair<Integer, Integer>(-1, -1);
+		return new Position(-1, -1);
+	}
+	
+	public class Position {
+	
+		public final int x;
+		public final int y;
+		
+		Position(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
 	}
 	
 }
