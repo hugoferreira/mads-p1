852dfff3a611a3f6f9f567522dda3388ea075ed1
Tiago Casanova <tiago.casanova.pt@gmail.com>
2012-09-26 18:57:58 +0100

    Basic structure

--- ns-group3/src/Tab.java
+++ ns-group3/src/Tab.java
@@ -1,4 +1,12 @@
+import java.awt.Point;
+import java.util.HashMap;
+
 
 public class Tab {
-
+	private HashMap<Point, Character> map = new HashMap<Point, Character>();
+	
+	public void change(int x, int y, char f){
+		Point position = new Point(x, y);
+		map.put(position, f);
+	}
 }
